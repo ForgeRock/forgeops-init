28f8220d9e32bac1b2f32d3ab9d9f4fefe8a8c30
Pavel Balcarek <pavel.balcarek@forgerock.com>
2019-01-22 17:04:52 +0100

    Fixes for IG benchmarks. Added token path var, changed how test works. (#154)
    - Fixes for IG benchmarks. Added token path var, changed how test works.
    - Tests can now be chained together to run one after another while using the tokens.csv file as file for access tokens.

--- 6.5/benchmarks/gatling-simulation-files/ig/IGAccessTokensSim.scala
+++ 6.5/benchmarks/gatling-simulation-files/ig/IGAccessTokensSim.scala
@@ -1,12 +1,12 @@
 package ig
-  
+
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 
-class IGAccessTokenSim extends Simulation {
+class IGAccessTokensSim extends Simulation {
 
   val concurrency: Integer = Integer.getInteger("concurrency", 10)
   val duration: Integer = Integer.getInteger("duration", 600)
@@ -17,14 +17,8 @@ class IGAccessTokenSim extends Simulation {
     
   val igUrl: String = igProtocol + "://" + igHost + ":" + igPort
   val random = new util.Random
-  
-  val csvFile = "/opt/CEBT/sync_folder/simulations/pavel/ig/tokens.csv"
-  
-  def getXOpenAMHeaders(username: String, password: String): scala.collection.immutable.Map[String, String] = {
-    scala.collection.immutable.Map(
-      "X-OpenAM-Username" -> username,
-      "X-OpenAM-Password" -> password)
-  }
+
+  val csvFile: String = System.getProperty("csv_file_path", "/tmp/tokens.csv")
   
   val httpProtocol: HttpProtocolBuilder = http
     .baseURLs(igUrl)
