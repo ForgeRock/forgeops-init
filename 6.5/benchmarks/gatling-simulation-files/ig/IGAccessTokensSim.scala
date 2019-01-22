14571fd54be16d949613632f2ddcbfc9647f9e8a
pavel.balcarek <pavel.balcarek@forgerock.com>
2019-01-22 17:03:31 +0100

    Fixed default urls

--- 6.5/benchmarks/gatling-simulation-files/ig/IGAccessTokensSim.scala
+++ 6.5/benchmarks/gatling-simulation-files/ig/IGAccessTokensSim.scala
@@ -11,7 +11,7 @@ class IGAccessTokensSim extends Simulation {
   val concurrency: Integer = Integer.getInteger("concurrency", 10)
   val duration: Integer = Integer.getInteger("duration", 600)
   val warmup: Integer = Integer.getInteger("warmup", 1)
-  val igHost: String = System.getProperty("ig_host", "login.prod.perf.forgerock-qa.com")
+  val igHost: String = System.getProperty("ig_host", "openig.prod.perf.forgerock-qa.com")
   val igPort: String = System.getProperty("ig_port", "443")
   val igProtocol: String = System.getProperty("ig_protocol", "https")
     
