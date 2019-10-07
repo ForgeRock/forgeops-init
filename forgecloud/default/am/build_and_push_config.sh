22fb411984f29462339dc6444e76dc97eea18be1
Craig McDonnell <craig.mcdonnell@forgerock.com>
2019-10-07 10:26:06 +0100

    add temporary Docker image build script

--- forgecloud/default/am/build_and_push_config.sh
+++ forgecloud/default/am/build_and_push_config.sh
@@ -3,13 +3,13 @@
 
 # temporary script for building am-config Docker image and pushing it to gcr
 
-docker build -t gcr.io/fr-saas-registry/am-config:FRAAS-1265-AM-FBC-43c870f -f am-config.Dockerfile .
+docker build -t gcr.io/fr-saas-registry/am-config:FRAAS-1265-AM-FBC-dadf2cc -f am-config.Dockerfile .
 
-docker push gcr.io/fr-saas-registry/am-config:FRAAS-1265-AM-FBC-43c870f
+docker push gcr.io/fr-saas-registry/am-config:FRAAS-1265-AM-FBC-dadf2cc
 
 # deploy this image as an init-container within the openam pod
 #- env:
-#  image: gcr.io/fr-saas-registry/am-config:FRAAS-1265-AM-FBC-43c870f
+#  image: gcr.io/fr-saas-registry/am-config:FRAAS-1265-AM-FBC-dadf2cc
 #  imagePullPolicy: Always
 #  name: config
 #  resources: {}
@@ -20,4 +20,4 @@ docker push gcr.io/fr-saas-registry/am-config:FRAAS-1265-AM-FBC-43c870f
 #    name: openam-root
 
 
-docker run -it --entrypoint ash gcr.io/fr-saas-registry/am-config:FRAAS-1265-AM-FBC-43c870f
+#docker run -it --entrypoint ash gcr.io/fr-saas-registry/am-config:FRAAS-1265-AM-FBC-dadf2cc
